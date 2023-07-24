6a29c0feb4a5ee19d67f0eccfc0cc24d3b308a9b
Albert Cordina <albert.cordina@dci-student.org>
2023-07-24 15:23:10 +0200

    an update of the IndexOf

--- IndexOf.java
+++ IndexOf.java
@@ -1,8 +1,10 @@
 public class IndexOf {
+    // indexOf() = method returns the position of the first occurrence of specified character(s) in a string.
+    // lastindexOf() = method returns the position of the last occurrence of specified character(s) in a string.
     public static void main(String[] args) {
-        String txt = "Please, locate where 'locate' occurs ";
-        System.out.println(txt.indexOf("locate"));
-
-
+        String txt = "Please, Please locate where 'locate' occurs ";
+        System.out.println(txt.indexOf("Please"));    //  prints out '0' (the starting position of the first 'Please');
+        // System.out.println(txt.lastIndexOf("Please")); prints out '8' (the starting position of the second 'Please');
+        //System.out.println(txt.indexOf("e", 15));  //  prints out the first 'e' after the 15th character in the string;
     }
 }
