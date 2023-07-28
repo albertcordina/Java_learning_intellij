d010c1ff64bffc54149aea4237cf1774d12d88ae
Albert Cordina <albert.cordina@dci-student.org>
2023-07-28 14:45:15 +0200

    an update of the files

--- Array_2D.java
+++ Array_2D.java
@@ -1,5 +1,30 @@
 public class Array_2D {
     public static void main(String[] args) {
         // 2D array = an array of arrays; 1:45;
+        String[][] cars = new String[3][3];
+        cars[0][0] = "Camaro"; // the first [] is row, the second [] is the position in the row/column;
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Silverado";
+        cars[1][0] = "Mustang";
+        cars[1][1] = "Ranger";
+        cars[1][2] = "F-150";
+        cars[2][0] = "Ferrari";
+        cars[2][1] = "BMW";
+        cars[2][2] = "Tesla";
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println();
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.print(cars[i][j] + " "); // i = row, j = column;
+            }
+        }
+
+        // another way:
+        // String[][] cars = {
+        //                      {"Camaro", "Corvette", "Silverado"},
+        //                      {"Mustang", "Ranger", "F-150"},
+        //                      {"Ferrari", "BMW", "Tesla"}
+        //                   };
+
     }
 }
--- Loop_For.java
+++ Loop_For.java
@@ -1,13 +1,12 @@
 public class Loop_For {
     public static void main(String[] args) {
 
-        // for = nesting loop
+        // for loop = executes a block of code a LIMITED amount of times; 1:28;
 
-     for (int i=0; i<10; i++){
-          for (int j=0; j<=1; j++){
-              String star = "*";
-              System.out.println(star);
-          }
-        }
+     // for (int i=0; i<10; i++) {System.out.println(i);}
+     for (int i=0; i<10; i+=2) {System.out.println(i);}
+   // the 3 statements:  i (shorten of "index") = DECLARATION;  i<10 = CONDITION;  i++ = INCREMENTATION or i-- = DECREMENTATION;
+
+        //  for (int j=0; j<=1; j++) {String star = "*"; System.out.println(star);} // loop inside the loop;
     }
 }
