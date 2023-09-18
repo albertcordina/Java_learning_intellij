98a76b08c70cb143b45bf08a12d78ad6fcf1d8c6
Albert Cordina <albert.cordina@dci-student.org>
2023-09-18 20:27:29 +0200

    update

---
+++ Up_And_Downcasting_rules.java
@@ -0,0 +1,55 @@
+class Animals {
+    /*
+     * Upcasting refers to typecasting a child object to a parent object.
+     * Downcasting provides casting a parent object to a child object.
+     */
+
+
+    String name;
+
+    public void makeNoise() {
+        System.out.println("I am just an Animal");
+    }
+}
+
+class Dogs extends Animals {  // the subclass of Animal;
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Woof woof!");
+    }
+
+    public void growl() {     //  this method exists only in a Dog class;
+        System.out.println("Grrrrrr");
+
+    }
+}
+
+public class Up_And_Downcasting_rules {
+
+    public static void main(String[] args) {
+//                                                         UPCASTING:
+
+        Animals animal = new Dogs(); // Upcasting Dog to Animal class; Creating a Child object and casting it to its Parent (easy);
+        // Java always allows us to treat an Object of a Child type as an Object of its Parent type;
+//      animal.growl();            // And yet, we do not have access to the method 'growl' in a Dog subclass;
+
+        doAnimalStuff(animal);     // the print-out is 'Woof woof!', because method 'makeNoise' is in the Animal and Dog classes;
+
+
+//                                                         DOWNCASTING:
+
+        Dogs dog = (Dogs) animal;   //  we tell Java that this Animal is a Dog;
+        dog.growl();              //  now the method 'growl' in Dog class is working;
+
+
+    }
+
+    public static void doAnimalStuff (Animals animal) {
+        animal.makeNoise();                         // the method 'makeNoise' is in the Animal and Dog classes;
+        if (animal instanceof Dogs){                 // with 'if' we also print out 'Grrrrrr', i.e the method 'growl' of Dog class;
+            Dogs dog = (Dogs) animal;
+            dog.growl();
+        }
+    }
+}
