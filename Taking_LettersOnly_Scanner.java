d8a6cd634e93253783d5a44cdf0adee64877cd87
Albert Cordina <albert.cordina@dci-student.org>
2023-11-27 22:29:08 +0100

    Update

---
+++ Taking_LettersOnly_Scanner.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Taking_LettersOnly_Scanner {
+
+    private static Scanner scanner = new Scanner (System.in);
+
+    public static void main(String[] args) {
+
+        String name = getLetter(scanner);
+        System.out.println("Your word is: " + name);
+
+        scanner.close();
+    }
+    /*
+     *  getLetter method to validate the input, i.e. to ensure that only letters are entered
+     */
+    private static String getLetter(Scanner scanner) {
+        System.out.print("Please, enter ...: ");
+        String input = scanner.nextLine();
+
+        while (!isAllLetters(input.trim())) {
+            System.out.print("Invalid input. Please enter only letters for ...: ");
+            input = scanner.nextLine();
+        }
+        // trim method is used to remove leading and trailing whitespace from the input.
+        return input.trim();
+    }
+    // the isAllLetters method checks whether the input consists only of letters.
+    private static boolean isAllLetters(String input) {
+        return !input.isEmpty() && input.chars().allMatch(Character::isLetter);
+    }
+}
