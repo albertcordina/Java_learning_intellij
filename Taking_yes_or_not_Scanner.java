e8de8d8b9ae84737786aa0ec0858c8c91ee96c5b
Albert Cordina <albert.cordina@dci-student.org>
2023-11-28 16:15:24 +0100

    Update

--- Taking_yes_or_not_Scanner.java
+++ Taking_yes_or_not_Scanner.java
@@ -6,12 +6,12 @@ public class Taking_yes_or_not_Scanner {
 
         System.out.println("We need a response from you. ");
 
-        choice(scanner);
-        System.out.println("Your choice: " + choice(scanner));
+        yesOrNot(scanner);
+        System.out.println("Your choice: " + yesOrNot(scanner));
         scanner.close();
     }
 
-    public static String choice (Scanner scanner) {
+    public static String yesOrNot (Scanner scanner) {
 
         System.out.print("Please, enter 'yes' or 'not': ");
         String input = scanner.next().toLowerCase();
