7f15b64ff2a5f1e1aa02d5bbafd3ee3644c0152e
Albert Cordina <albert.cordina@dci-student.org>
2023-11-28 15:49:26 +0100

    Update

--- Taking_yes_or_not_Scanner.java
+++ Taking_yes_or_not_Scanner.java
@@ -8,34 +8,19 @@ public class Taking_yes_or_not_Scanner {
 
         choice(scanner);
         System.out.println("Your choice: " + choice(scanner));
+        scanner.close();
     }
-        /*
-        String name = scanner.nextLine();
-
-//       --- Requesting the user a certain answer 'Yes' or 'Not' ---
-
-        System.out.print("Are you married? (Enter 'yes' or 'not'): ");
-        String mar_status = scanner.nextLine().toLowerCase();
-
-        while (!mar_status.equals("yes") && !mar_status.equals("not")) {
-            System.out.print("Please, enter 'yes' or 'not': ");
-            mar_status = scanner.next().toLowerCase();
-
-        }
-        // scanner.close();
-    } */
-
 
     public static String choice (Scanner scanner) {
 
-    		System.out.print("Please, enter 'yes' or 'not': ");
-    String choice = scanner.next().toLowerCase();
+        System.out.print("Please, enter 'yes' or 'not': ");
+        String input = scanner.next().toLowerCase();
 
-		while (!choice.equals("yes") && !choice.equals("not")) {
+		while (!input.equals("yes") && !input.equals("not")) {
             System.out.print("Please, enter 'yes' or 'not': ");
-            choice = scanner.next().toLowerCase();
+            input = scanner.next().toLowerCase();
         }
-        return choice;
+        return input;
     }
 }
 
