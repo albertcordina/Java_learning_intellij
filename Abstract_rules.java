94532e75ad13901aef6e3af04166d06067dfc099
Albert Cordina <albert.cordina@dci-student.org>
2023-12-11 22:38:03 +0100

    Update

--- Abstract_rules.java
+++ Abstract_rules.java
@@ -1,4 +1,9 @@
-abstract class A { // abstract class can not be instantiated (called in another class); It must be inherited from another class; It is similar to 'interface';
+/*
+ * abstract class can not be called in another class,
+ * It must be inherited from another class.
+ * It is similar to 'interface'.
+ */
+abstract class A {
 
     /*
      * Abstract class: is a restricted class that cannot be used to create objects (NOTE: to access it, it must be inherited from another class).
@@ -7,16 +12,42 @@ abstract class A { // abstract class can not be instantiated (called in another 
      * It can have 'final' methods which forces the subclass not to change the body of the method;
      */
 
-    abstract void greet(); // abstract method does not have a body;
-    // abstract method can not be created in a non-abstract class;
+    abstract void greet(); // abstract method does not have a body.
+    // abstract method can not be created in a non-abstract class.
 
-    public final void run () {
+    public final void run () { // abstract class can also have an ordinary methods
         System.out.println("running...");
     }
-
 }
-public class Abstract_rules {
+
+/*
+ * NOTE:  when we 'extends' the abstract class, the abstract method must be implemented !
+ */
+class BB extends A  {
+    /*
+     * you cannot create an instance of an abstract class directly because it may have abstract methods
+     * that need to be implemented by its subclasses. However, you can call a non-abstract method of
+     * an abstract class by creating an instance of a concrete subclass and then invoking the method, i.e. 'run'.
+     */
+
+    @Override
+    public void greet() {
+        System.out.println("Hallo !");
+
+    }
+}
+/*
+ * Here we call a non-abstract method of an abstract class by creating an instance
+ * of a concrete subclass, i.e. Class 'BB' and then invoking also the method 'run' (see below):
+ */
+public class Abstract_rules   {
+
     public static void main(String[] args) {
+
+        BB b = new BB();
+
+        b.greet();
+        b.run();
 
     }
 }
